3d3a3bdb4dce18f2527406fe6e56ce2b1bd2995c
machelle-2003 <52976924+machelle-2003@users.noreply.github.com>
2019-07-16 15:39:38 -0400

    add files via upload

---
+++ mints.js
@@ -0,0 +1,20 @@
+
+function toggleSection(id){
+  //first get dom element specified by name
+  var elt=document.getElementById(id)
+  if(elt.style.visibility==='hidden'){
+    elt.style.visibility= 'visible';
+  }else{
+    elt.style.visibility='hidden';
+  }
+
+  
+
+
+  //if the elementis hidden,show it
+
+
+  //if the element is showing,hhide it
+
+
+}
